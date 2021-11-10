327a87318fe8645c3922cfdb8eb565b57b55edff
JoseEmmanuelHerrera <83984787+JoseEmmanuelHerrera@users.noreply.github.com>
2021-11-10 10:32:38 -0600

    Add files via upload

---
+++ src/routes/candies.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router  = express.Router();
+const pool = require('../database.js');
+
+router.get('/', async(req, res) =>{
+    let listCandies = await pool.query('SELECT * FROM candies');
+    res.json({
+        status: 1,
+        message: "Se ha listado correctamente",
+        listCandies: listCandies
+    });
+}) 
+
+router.get('/:id', async(req, res) => {
+    const { id } = req.params;
+    let candy = await pool.query('SELECT * FROM candies WHERE id = ?', [id]);
+    res.json({
+        status: 200,
+        message: "Se ha listado correctamente",
+        candy:candy
+    });
+
+});
+
+router.post('/create', async(req, res) => {
+const { name, price, expiration, isSalad, date_registered, date_created } = req.body;
+const candy = {
+    name, price, expiration, isSalad, date_registered, date_created, status: 1
+};
+    await pool.query('INSERT INTO candies set ?', [candy]);
+    res.json({
+        status: 200,
+        message: "Se ha creado correctamente",
+        candy:candy
+    });
+
+});
+
+router.post('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, price, expiration, isSalad, date_registered, date_created } = req.body;
+
+    const candy = { name, price, expiration, isSalad, date_registered, date_created };
+
+    pool.query('UPDATE candies SET ? WHERE  id = ?', [candy, id]);
+    res.json({
+        status: 200,
+        message: "Se ha actualizado correctamente",
+        candy:candy
+    });
+});
+
+router.post('/delete/:id', async(req, res) =>{
+    const { id } = req.params;
+
+   await pool.query('UPDATE candies SET status = 0 WHERE id = ?', [id]);
+   res.json({
+       status: 200,
+       message: "Se ha eliminado corectamente"
+   });
+});
+
+
+module.exports = router;
